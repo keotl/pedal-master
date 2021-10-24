3e6001aeb7b45e4fd908407f1261300375c1505b
Kento A. Lauzon <kento.lauzon@ligature.ca>
2021-10-24 18:52:36 -0400

    directory cleanup rev2

--- firmware_rev2.ino
+++ firmware_rev2.ino
@@ -1,15 +1,21 @@
 #include <Keyboard.h>
 
+// CONFIG
+
+// Set button mapping here
 const int BUTTONS[3] = {3,4,5};
 const int KEYS[3] = {KEY_LEFT_CTRL, KEY_LEFT_ALT, KEY_LEFT_SHIFT};
+
+// Set base polarity here for each pedal.
+// Inverted pedals send 5V when open, 0V when closed. (e.g. Yamaha FC4A)
 const bool INVERTED[3] = {true,false,false};
 
+// FIRMWARE
 int buttonStates[3] = {0,0,0};
 bool keyboardStates[3] = {false, false, false};
 
-
 void incrementState(int i) {
-  if (buttonStates[i] < 10) {
+  if (buttonStates[i] < 1) {
     buttonStates[i]++;
   }
 }
@@ -39,10 +45,10 @@ void readButton(int i) {
 
 void updateKeyboard() {
   for (int i = 0; i < 3; i++) {
-    if (buttonStates[i] >= 5 && !keyboardStates[i]) {
+    if (buttonStates[i] > 0 && !keyboardStates[i]) {
       keyboardStates[i] = true;
       Keyboard.press(KEYS[i]);
-    } else if (buttonStates[i] < 5 && keyboardStates[i]) {
+    } else if (buttonStates[i] < 1 && keyboardStates[i]) {
       keyboardStates[i] = false;
       Keyboard.release(KEYS[i]);
     }
